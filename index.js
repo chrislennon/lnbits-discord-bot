d0c2ca1c98c0819eb3da85fdcc110dc6a564f659
Chris Lennon <chrislennon@users.noreply.github.com>
2021-07-10 12:00:40 +0200

    add logger, override console

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 const Bot = require(`./Bot`);
+const winston = require('winston');
+
+const logDate = new Date().toISOString();
+var fs = require( 'fs' );
+var logDir = 'logs';
+if ( !fs.existsSync( logDir ) ) {
+   fs.mkdirSync( logDir );
+}
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({ filename: `./${logDir}/${logDate}_error.log`, level: 'error' }),
+    new winston.transports.File({ filename: `./${logDir}/${logDate}_combined.log` }),
+  ],
+});
+
+logger.add(new winston.transports.Console({
+  format: winston.format.simple(),
+}));
+
+console.log = function(){
+  return logger.info.apply(logger, arguments)
+}
+console.error = function(){
+  return logger.error.apply(logger, arguments)
+}
+console.info = function(){
+  return logger.warn.apply(logger, arguments)
+}
 
 const DiscordBot = new Bot();
 
 // Handle graceful shutdowns
-process.on(`SIGINT`, cleanup);
-process.on(`SIGTERM`, cleanup);
+// process.on(`SIGINT`, cleanup);
+// process.on(`SIGTERM`, cleanup);
 
 function cleanup() {
   DiscordBot.destroy();
